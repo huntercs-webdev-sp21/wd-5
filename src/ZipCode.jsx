8306af91d849d71a6f7d96a5ee193e83d12dc065
Jasper Cheung <23638735+JasperCheung@users.noreply.github.com>
2021-04-08 19:45:10 -0400

    error trigger build

--- src/ZipCode.jsx
+++ src/ZipCode.jsx
@@ -14,7 +14,7 @@ class ZipCode extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    axios.get(`https://ctp-zip-api.herokuapp.com/zip/${this.state.zip}`)
+    axios.get(`https://ctp-zip-api.herokuapp.com/zisp/${this.state.zip}`)
       .then(response => {
         const newData = response.data;
         this.setState({data: newData});
